546d14922d8992c161020947b18140c304ab04c9
Silence Tang <357003671@qq.com>
2020-02-12 13:57:21 +0800

    add offline prompt

--- src/component/warning.js
+++ src/component/warning.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './warning.scss'
 import { ReactComponent as WarningImage } from '../image/bit38_decode_pay_attention.svg'
+import  useInterval from '../utils/useInterval'
+import onlineWifiIcon from '../image/online_notrecommend@2x.png'
+import offlineWifiIcon from '../image/offline_recommended@2x.png'
 export default (props) => {
+  const [isOnline, setIsOnline] = useState(false)
+  useInterval(() => {
+    console.log(window.navigator.onLine)
+    if (window.navigator.onLine) {
+      setIsOnline(true)
+    } else {
+      setIsOnline(false)
+    }
+  }, 1000)
   return (
     <div>
       <div className="warning">
@@ -10,6 +22,17 @@ export default (props) => {
           <div className="warningTitle"> {props.title}</div>
           <div className="warningDescription">
             {props.content}
+          </div>
+          <div className="network__wraper">
+            
+            <div>Network Connection Status:
+              <img src={isOnline ? onlineWifiIcon : offlineWifiIcon} />
+              {isOnline ? 'Online' : 'Offline'}
+            <span
+              style={{ background: isOnline ? '#e37f0a' : '#28ab00' }}
+            >{isOnline ? 'Not Recommended' : 'Recommended'}</span>
+            </div>
+            {isOnline ? <div>(Suggest to switch to offline mode for security reason.)</div> : ''}
           </div>
         </div>
       </div>
---
+++ src/utils/useInterval.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react'
+export default function(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
